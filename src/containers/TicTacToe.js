228a84c9d5137c0ca3543e7d70e29c0c440fc2ce
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-04-09 18:15:37 +0300

    building boards

--- src/containers/TicTacToe.js
+++ src/containers/TicTacToe.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
 import { Stage } from 'react-konva'
+import { Board } from '../styled/TicTacToe'
 
 class TicTacToe extends Component {
   state = {
-    rows: 3
+    rows: 3,
+    gameState: new Array(9).fill(false),
+    ownMark: 'X',
+    otherMark: 'O',
+    gameOver: false,
+    yourTurn: true,
+    winner: false,
+    win: false
   }
 
   componentDidMount() {
@@ -11,9 +19,9 @@ class TicTacToe extends Component {
     let width = window.innerWidth
     let size = (height < width) ? height * .8 : width * .8
     let rows = this.state.rows
-    let units = size / rows
+    let unit = size / rows
 
-    this.setState({size, rows, units})
+    this.setState({size, rows, unit})
   }
 
   move = () => {
@@ -28,13 +36,23 @@ class TicTacToe extends Component {
 
   
   render() {
+    let {
+      size,
+      unit,
+      rows
+    } = this.state
+    
     return (
       <div>
         <Stage
-          width={this.state.size}
-          height={this.state.size}
+          width={size}
+          height={size}
         >  
-          {/*<Board />*/}
+          {<Board
+            unit={unit}
+            rows={rows}
+            size={size}
+          />}
           {/*<Squares />*/}
         </Stage>  
       </div>
---
+++ src/styled/TicTacToe.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Layer, Line } from 'react-konva'
+
+export const Board = ({ unit, size, rows }) => {
+  let stroke = 'grey'
+  let strokeWidth = 10
+
+  const grid = [...new Array(rows - 1).keys()]
+    .reduce((acc, el) => {
+      const position = unit * (el + 1)
+      acc.push(
+        <Line
+          points={[position, 0, position, size]}
+          stroke={stroke}
+          strokeWidth={strokeWidth}
+          key={el+'v'}
+        />
+      )
+      acc.push(
+        <Line
+          points={[0, position, size, position]}
+          stroke={stroke}
+          strokeWidth={strokeWidth}
+          key={el+'h'}
+        />
+      )
+      return acc
+    }, [])
+
+  return (
+    <Layer>
+      {grid}
+    </Layer>
+  )
+}
