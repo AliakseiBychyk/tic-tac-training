39b92eafbc62933f23b5cd6bc6037526a7d6a903
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-04-09 16:57:52 +0300

    canvas setup

---
+++ src/containers/TicTacToe.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { Stage } from 'react-konva'
+
+class TicTacToe extends Component {
+  state = {
+    rows: 3
+  }
+
+  componentDidMount() {
+    let height = window.innerHeight
+    let width = window.innerWidth
+    let size = (height < width) ? height * .8 : width * .8
+    let rows = this.state.rows
+    let units = size / rows
+
+    this.setState({size, rows, units})
+  }
+
+  move = () => {
+
+  }
+
+  makeAiMove = () => { }
+  
+  turingTest = () => { }
+  
+  recordGame = () => { }
+
+  
+  render() {
+    return (
+      <div>
+        <Stage
+          width={this.state.size}
+          height={this.state.size}
+        >  
+          {/*<Board />*/}
+          {/*<Squares />*/}
+        </Stage>  
+      </div>
+    )
+  }    
+}
+export default TicTacToe
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Template from '../containers/Template'
-import Home from '../containers/Home'
+import TicTacToe from '../containers/TicTacToe'
 import Profile from '../containers/Profile'
 
 const Routes = () => (
   <Template>
     <Switch> 
-      <Route path='/' exact component={Home} />
+      <Route path='/' exact component={TicTacToe} />
       <Route path='/profile' component={Profile} />
     </Switch>
   </Template>
