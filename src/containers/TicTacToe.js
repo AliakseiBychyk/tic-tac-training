abc15a32062c5354978e24326ac57c1a17a43426
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-04-10 13:47:09 +0300

    building AI

--- src/containers/TicTacToe.js
+++ src/containers/TicTacToe.js
@@ -4,6 +4,21 @@ import { Board, Squares } from '../styled/TicTacToe'
 import { access } from 'fs';
 
 class TicTacToe extends Component {
+
+  constructor(props) {
+    super(props)
+    this.combos = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ]
+  }
+
   state = {
     rows: 3,
     gameState: new Array(9).fill(false),
@@ -40,11 +55,37 @@ class TicTacToe extends Component {
     }
   }
 
-  move = (marker, index) => {
-    console.log('Move me!', marker, index)
+  move = (index, marker) => {
+    console.log('Move me!', index, marker)
   }
 
-  makeAiMove = () => { }
+  makeAiMove = (gameState) => { 
+    const otherMark = this.state.otherMark
+    const openSquares = gameState.reduce((acc, square, i) => {
+      return !square ? acc.push(i) : acc
+    }, [])
+    const aiMove = openSquares[this.random(0, openSquares.length)]
+    this.move(aiMove, otherMark)
+  }
+
+  random = (min, max) => {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min
+  }
+
+  /**
+   * checks if the marked fields match to winning combos
+   * @param {Array} array of booleans 
+   */
+  winChecker = (gameState) => {
+    const combos = this.combos
+    return combos.find(combo => {
+      const [a, b, c] = combo
+      return (
+        gameState[a] === gameState[b] && gameState[a] === gameState[c] && gameState[a])
+    })
+  }
   
   turingTest = () => { }
   
