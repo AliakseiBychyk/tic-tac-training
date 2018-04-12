35473ff4c107425ea345845169d9a6aab2b89a8b
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-04-12 16:43:26 +0300

    Profile page styles

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { Component } from 'react'
+import {
+  Container,
+  Name,
+  GameListHeader,
+  GameList,
+  GameRecord,
+  Column,
+  ColumnLabels
+} from '../styled/Profile'
 
-const Profile = () => (
-  <div>
-    <h1>Profile</h1>
-  </div>
-)
+class Profile extends Component{
+  static defaultProps = {
+    user: {
+      email: 'USER_EMAIL',
+      games: [
+        {
+          winner: true,
+          createdAt: '04/10/2018',
+          id: '0001'
+        },
+        {
+          winner: true,
+          createdAt: '04/11/2018',
+          id: '0002'
+        },
+        {
+          winner: true,
+          createdAt: '04/12/2018',
+          id: '0003'
+        }
+      ]
+    }
+  }
+
+  render() {
+    let {email} = this.props.user
+    return (
+      <Container>
+        <Name>{email}</Name>
+        <GameList>
+          <GameListHeader>My Games</GameListHeader>
+          <ColumnLabels>
+            <Column>
+              Outcome
+            </Column>
+            <Column>
+              Guess
+            </Column>
+            <Column>
+              Guessed Correctly
+            </Column>
+            <Column>
+              Date
+            </Column>
+          </ColumnLabels>
+        </GameList>    
+      </Container>
+    )
+  }  
+}
 
 export default Profile
